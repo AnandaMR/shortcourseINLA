7b3e2f37a0d934e6528bb192bf8c8bff2530f662
Elias T Krainski <eliaskrainski@gmail.com>
2026-03-10 22:30:57 +0300

    update day 2 code

--- rcode/mInfant_data_map.R
+++ rcode/mInfant_data_map.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+    
 
 setwd(here::here(""))
 getwd()
@@ -18,7 +20,7 @@ iobt <- read.csv2(
 head(iobt,2)
 tail(iobt,2)
 
-jj <- 2:(ncol(iobt)-1)
+(jj <- 2:(ncol(iobt)-1))
 years <- as.integer(substr(
     colnames(iobt)[jj], 2, 5))
 years
@@ -41,8 +43,6 @@ if(!file.exists("data/mInfant.rds")) {
     
     str(mInfant)
     
-    library(ggplot2)
-    
     ggplot(mInfant) + theme_minimal() +
         geom_line(aes(x = year, y = iobt/nasc, color = mun))
 
@@ -62,7 +62,6 @@ if(file.exists("maps/mg_mun.shp")) {
         showProgress = TRUE
     )
     st_write(mg_mun, "maps/mg_mun.shp")
-    dir("maps")
 }
 
 head(mg_mun)
@@ -77,8 +76,14 @@ o2 <- pmatch(substr(iobt$Mun, 1, 6),
 summary(o2)
 nasc[is.na(o2),]
 
-mg_mun$nasc <- nasc$Total[o1[complete.cases(o1)]]
-mg_mun$iobt <- iobt$Total[o2[complete.cases(o2)]]
+names(nasc)
+
+mg_mun$nasc <- nasc[o1[complete.cases(o1)], 30]
+mg_mun$iobt <- iobt[o2[complete.cases(o2)], 30]
+
+head(mg_mun, 3)
+
+summary(mg_mun$nasc)
 
 bb <- st_bbox(mg_mun)
 bb
@@ -98,3 +103,133 @@ ggplot(mg_mun) + theme_minimal() +
 dev.off()
 
 system("eog iobt_map_mg.png &")
+
+
+library(spdep)
+
+viz <- poly2nb(mg_mun)
+
+viz
+
+xy <- st_coordinates(st_centroid(mg_mun))
+
+par(mar = c(0,0,0,0))
+plot(viz, xy)
+
+nb2INLA("grafo", viz)
+
+## modelo y_i ~ Poisson(r_i * N_i)
+
+ff1 <- iobt ~
+    f(area, model = "besag", graph = "grafo",
+      scale.model = TRUE)
+
+library(INLA)
+
+mg_mun$area <- 1:nrow(mg_mun)
+
+fit1 <- inla(
+    formula = ff1,
+    family = "poisson",
+    data = st_drop_geometry(mg_mun)[c("nasc", "iobt", "area")],
+    E = nasc,
+    control.predictor = list(link = 1)
+)
+
+fit1$summary.fixed
+
+exp(-4.09)
+
+plot(fit1$marginals.random$area[[1]], type = "l", lwd = 5, xlim = c(-1,1))
+lines(fit1$marginals.random$area[[2]], col = 2, lwd = 5)
+lines(fit1$marginals.random$area[[3]], col = 3, lwd = 5)
+lines(fit1$marginals.random$area[[4]], col = 4, lwd = 5)
+
+mg_mun$log_ri <- fit1$summary.random$area$mean
+
+ggplot(mg_mun) + theme_minimal() +
+    geom_sf(aes(fill = log_ri)) +
+    scale_fill_distiller(
+        palette = "RdBu"
+    )
+
+### lambda0 = sum_i obitos_i / sum_i nascidos_i
+lambda0 <- sum(mg_mun$iobt, na.rm = TRUE) / sum(mg_mun$nasc)
+lambda0
+
+mg_mun$E <- lambda0 * mg_mun$nasc
+
+### \lambda = E * exp( preditor linear )
+
+fit2 <- inla(
+    formula = ff1,
+    family = "poisson",
+    data = st_drop_geometry(mg_mun)[c("nasc", "iobt", "area", "E")],
+    E = E,
+    control.predictor = list(link = 1)
+)
+
+fit2$summary.fixed
+
+mg_mun$log_smr <- fit2$summary.random$area$mean
+
+ggplot(mg_mun) + theme_minimal() +
+    geom_sf(aes(fill = log_smr)) +
+    scale_fill_distiller(
+        palette = "RdBu"
+    )
+
+names(inla.models())
+names(inla.models()$latent)
+## inla.models()$latent$besagproper
+inla.doc("besagproper")
+
+
+ff3 <- iobt ~
+    f(area, model = "besagproper", graph = "grafo")
+
+fit3 <- inla(
+    formula = ff3,
+    family = "poisson",
+    data = st_drop_geometry(mg_mun)[c("nasc", "iobt", "area", "E")],
+    E = E,
+    control.predictor = list(link = 1)
+)
+
+fit3$summary.fixed
+
+mg_mun$log_smr3 <- fit3$summary.random$area$mean
+
+ggplot(mg_mun) + theme_minimal() +
+    geom_sf(aes(fill = log_smr3)) +
+    scale_fill_distiller(
+        palette = "RdBu"
+    )
+
+
+fit2$summary.hyperpar
+fit3$summary.hyperpar
+
+## bym2 :  ( sqrt(\phi) * u_i + sqrt(1- \phi) * v_i) / sqrt(tau)
+##  \phi : quao forte eh a dependencia espacial
+ff4 <- iobt ~
+    f(area, model = "bym2", graph = "grafo")
+
+fit4 <- inla(
+    formula = ff4,
+    family = "poisson",
+    data = st_drop_geometry(mg_mun)[c("nasc", "iobt", "area", "E")],
+    E = E,
+    control.predictor = list(link = 1)
+)
+
+fit4$summary.hyper
+
+mg_mun$log_smr4 <- fit3$summary.random$area$mean[1:nrow(mg_mun)]
+
+ggplot(mg_mun) + theme_minimal() +
+    geom_sf(aes(fill = log_smr4)) +
+    scale_fill_distiller(
+        palette = "RdBu"
+    )
+
